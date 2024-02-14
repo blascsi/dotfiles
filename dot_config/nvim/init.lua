aa5aca1dd627e2619cb1114e5ac391e8bf0bc9ca
Attila Blascsak <attila@blascsak.com>
2024-02-14 23:25:46 +0000

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -98,7 +98,7 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
+    main = "ibl,
     opts = {
       char = '┊',
       show_trailing_blankline_indent = false,
